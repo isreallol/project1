cefc7efa8da5a348baac6e7ea2a8e4999f1e592d
Tiqon Lediard Jr. <lediardtiqon@gmail.com>
2025-09-24 13:45:51 -0400

    XII

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 document.addEventListener("DOMContentLoaded", animation);
 
 function animation(){
-    /*gsap.from('#title', {
-        y:-40,
-        ease: 'bounce.out',
-        duration: 3,
-        onComplete: function(){gsap.to('#title',{
-            opacity: 0,
-            easein: 'power1,out',
-            duration: 4,
-        })}
-    })
-
-    gsap.from('#subtitle', {
-        y:-20,
-        ease: 'bounce.out',
-        duration: 3,
-        onComplete: function(){gsap.to('#subtitle',{
-            opacity: 0,
-            easein: 'power1,out',
-            duration: 7,
-        })}
-    })*/
-
-    gsap.from("#title", {
+    /*gsap.from("#title", {
         opacity: 0,
         easein: 'power1,out',
         duration: 1.5,
@@ -31,8 +9,8 @@ function animation(){
 
     gsap.to('#title',{
         y: 20,
-            ease: 'bounce.out',
-            duration: 1.5,
+        ease: 'bounce.out',
+        duration: 1.5,
     })
 
     gsap.from("#subtitle", {
@@ -40,18 +18,43 @@ function animation(){
         easein: 'power1,out',
         duration: 2,
     })
+    gsap.to("#title", {
+        y: -30,
+        opacity: 0,
+        duration: 1.5,
+        ease: "power1.in"
+    });
 
     gsap.to("#subtitle", {
-        x: 390,
+        y: -20,
+        opacity: 0,
         duration: 2,
+        ease: "power1.in",
+        delay: 0.5
+    });*/
+
+    gsap.from("#title", {
+        scale: 0.8,
+        opacity: 0,
+        duration: 1.5,
+        ease: "back.in(1.5)"
+    });
+
+    gsap.from("#subtitle", {
+        scale: 0.8,
+        opacity: 0,
+        duration: 2,
+        ease: "back.in(1.5)",
+        delay: 0.5,
         onComplete: function(){gsap.to('#options',{
             duration: 1,
             opacity: 1,
             delay: 0.5,
-            stagger: 0.1, // stagger in from the left with a 0.1 second gap in between animations
+            stagger: 0.2, // stagger in from the left with a 0.2 second gap in between animations
+            y: -10,
             ease: "sine.out"
         })}
-    })
+    });
 
     //LEFTOVERS
     gsap.from("#leftover_title", {
@@ -99,6 +102,14 @@ function animation(){
         x: 390,
         duration: 2.5,
     })
+
+    gsap.from(".main-image", {
+        opacity: 0,
+        scale: 0.8,
+        duration: 2,
+        delay: 1,
+        ease: "power2.out"
+    });
 
 }
 
